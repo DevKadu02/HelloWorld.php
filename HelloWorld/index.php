709f2a7cf448bac094b29909ad81f1e5b892d49d
Carlos Eduardo <101615010+DevKadu02@users.noreply.github.com>
2024-07-01 17:00:40 -0300

    Add files via upload

---
+++ HelloWorld/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Exemplo PHP</title>
+</head>
+<body>
+
+<?php
+
+$mensagem = "Olá, mundo! Este é um exemplo simples de PHP.";
+
+
+echo "<h1>$mensagem</h1>";
+?>
+
+</body>
+</html>
